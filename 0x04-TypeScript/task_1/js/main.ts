3907bc8028fb6fb817eac8f1f88657feb869d066
Don Bosco <don965877@gmail.com>
2024-12-15 01:20:43 +0300

    create director interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,4 @@
-interface Teacher  {
+interface Teacher {
     firstName: string,
     lastName: string,
     fullTimeEmployee: boolean,
@@ -14,7 +14,25 @@ const teacher1: Teacher = {
     location: "Phuket",
     contract: true, //New attribute
     department: "production"
-    
+
 }
 
 console.log(teacher1);
+
+interface Directors {
+   firstName: string,
+   lastName: string,
+   location: string,
+   fullTimeEmployee: boolean,
+   numberOfReports: number,
+   [propName: string]: any; //To allow additional attributes
+}
+
+    const director1: Directors = {
+        firstName: 'Don',
+        lastName: 'Bosco',
+        location: 'Singapore',
+        fullTimeEmployee: true,
+        numberOfReports: 10,
+    }
+    console.log(director1);
